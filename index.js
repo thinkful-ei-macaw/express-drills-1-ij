c0b9e23c369af63c4365218baea738badb1dfd23
Jack Lansing <john.a.lansing@gmail.com>
2020-03-23 14:44:01 -0400

    exercise 1 complete

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.get('/sum', (req, res) => {
+  const { a } = req.query;
+  const { b } = req.query;
+  
+  const sum = parseInt(a) + parseInt(b);
+
+  res.send(`The sum of ${a} and ${b} is ${sum}`);
+
+});
+
 app.listen(8000, () => console.log('Listening on port 8000'));
